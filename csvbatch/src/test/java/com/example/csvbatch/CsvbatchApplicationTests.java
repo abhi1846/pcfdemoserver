f9e844fddd8283344f80799065935ab618335c98
abhi1846 <singhabhishek846@gmail.com>
2019-10-19 19:37:35 +0530

    batch

---
+++ csvbatch/src/test/java/com/example/csvbatch/CsvbatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csvbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvbatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
